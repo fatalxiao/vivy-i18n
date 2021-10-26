9e1b117bc04ec0cb8116abc404a28f2e5f7b1c01
liangxiaojun <fatalxiao@163.com>
2021-10-26 15:34:22 +0800

    update example

--- examples/withI18nPlugin/src/index.js
+++ examples/withI18nPlugin/src/index.js
@@ -4,53 +4,33 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {createBrowserHistory} from 'history';
-import {renderRoutes} from 'react-router-config';
 import {Provider} from 'react-redux';
-import {ConnectedRouter} from 'connected-react-router';
-import {configureRoutes} from './routes';
 
-// Models
+// Sync component and model
+import Root from './modules/Root/containers/Root';
 import root from './modules/Root/models/root';
-import a from './modules/A/models/a';
-import b from './modules/B/models/b';
-import c from './modules/C/models/c';
 
 // Import Vivy
 import Vivy from 'vivy';
-import VivyRouter from 'vivy-router';
-import vivySubscription from 'vivy-subscription';
-
-// Create browser history
-const history = createBrowserHistory();
+import VivyI18n from '../../../src/index';
 
 // Create vivy
 const vivy = Vivy();
 
-// Apply router plugin
-vivy.use(VivyRouter({
-    history
-}));
-
-// Apply subscription plugin
-vivy.use(vivySubscription());
+// Apply I18n plugin
+vivy.use(VivyI18n());
 
 // Create store after configuration
 const store = vivy.createStore();
 
 // Register vivy models
 store.registerModels([
-    root,
-    a,
-    b,
-    c
+    root
 ]);
 
 render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
-            {renderRoutes(configureRoutes())}
-        </ConnectedRouter>
+        <Root/>
     </Provider>,
     document.getElementById('app-container')
 );
