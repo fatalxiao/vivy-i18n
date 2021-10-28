007ee5d7cc6e2ca6e6088907e8f245ea9bf83983
liangxiaojun <fatalxiao@163.com>
2021-10-28 10:17:56 +0800

    update example

--- examples/withI18nPlugin/src/modules/Form/containers/Form.js
+++ examples/withI18nPlugin/src/modules/Form/containers/Form.js
@@ -2,29 +2,84 @@
  * @file Form.js
  */
 
-import React from 'react';
+import React, {useCallback} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 // Components
 import {I18n} from 'vivy-i18n';
 
-const Form = () => (
-    <form>
+const Form = ({
+    userName, password,
+    dispatch
+}) => {
 
-        <p>
-            <label>
-                <I18n index="form/userName"/>
-                <input/>
-            </label>
-        </p>
+    /**
+     * Update user name to state when changed
+     * @type {(function(*): void)|*}
+     */
+    const handleUserNameChange = useCallback(e => {
+        dispatch?.({
+            type: 'form/updateUserName',
+            userName: e.target.value
+        });
+    }, [
+        dispatch
+    ]);
 
-        <p>
-            <label>
-                <I18n index="form/password"/>
-                <input/>
-            </label>
-        </p>
+    /**
+     * Update password to state when changed
+     * @type {(function(*): void)|*}
+     */
+    const handlePasswordChange = useCallback(e => {
+        dispatch?.({
+            type: 'form/updatePassword',
+            password: e.target.value
+        });
+    }, [
+        dispatch
+    ]);
 
-    </form>
-);
+    return (
+        <form>
 
-export default Form;
+            <p>
+                <label>
+                    <I18n index="form/userName"/>
+                    <input value={userName}
+                           onChange={handleUserNameChange}/>
+                </label>
+            </p>
+
+            <p>
+                <label>
+                    <I18n index="form/password"/>
+                    <input value={password}
+                           onChange={handlePasswordChange}/>
+                </label>
+                {
+                    password?.length < 8 ?
+                        <I18n index="form/passwordErrorMsg"/>
+                        :
+                        null
+                }
+            </p>
+
+        </form>
+    );
+
+};
+
+Form.propTypes = {
+
+    userName: PropTypes.string,
+    password: PropTypes.string,
+
+    dispatch: PropTypes.func
+
+};
+
+export default connect(state => ({
+    userName: state.form.userName,
+    password: state.form.password
+}))(Form);
