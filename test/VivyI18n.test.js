66ad1f029a891b6c96575caeecf77ca65de78099
liangxiaojun <fatalxiao@163.com>
2022-03-10 14:34:40 +0800

    update unit test

--- test/VivyI18n.test.js
+++ test/VivyI18n.test.js
@@ -9,11 +9,10 @@ import testModel from './mocks/testModel';
 test('Use Vivy I18n', () => {
 
     const vivy = Vivy();
-
     vivy.use(VivyI18n());
 
+    // eslint-disable-next-line no-unused-vars
     const store = vivy.createStore();
-    store.registerModel(testModel);
 
     expect(
         typeof translate
@@ -23,7 +22,7 @@ test('Use Vivy I18n', () => {
 
 });
 
-test('Use Vivy I18n by translate', () => {
+test('Use Vivy I18n translate title', () => {
 
     const vivy = Vivy();
 
@@ -36,6 +35,23 @@ test('Use Vivy I18n by translate', () => {
         translate('testModel/title')
     ).toEqual(
         testModel.i18ns['en-US'].title
+    );
+
+});
+
+test('Use Vivy I18n translate text', () => {
+
+    const vivy = Vivy();
+
+    vivy.use(VivyI18n());
+
+    const store = vivy.createStore();
+    store.registerModel(testModel);
+
+    expect(
+        translate('testModel/text')
+    ).toEqual(
+        testModel.i18ns['en-US'].text
     );
 
 });
