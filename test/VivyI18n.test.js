908aa7165e10205672894c5ddf1548f6c62c1f9d
liangxiaojun <fatalxiao@163.com>
2022-03-10 14:36:52 +0800

    update unit test

--- test/VivyI18n.test.js
+++ test/VivyI18n.test.js
@@ -22,7 +22,7 @@ test('Use Vivy I18n', () => {
 
 });
 
-test('Use Vivy I18n translate title', () => {
+test('Use Vivy I18n translate default en title', () => {
 
     const vivy = Vivy();
 
@@ -39,7 +39,7 @@ test('Use Vivy I18n translate title', () => {
 
 });
 
-test('Use Vivy I18n translate text', () => {
+test('Use Vivy I18n translate default en text', () => {
 
     const vivy = Vivy();
 
@@ -52,6 +52,44 @@ test('Use Vivy I18n translate text', () => {
         translate('testModel/text')
     ).toEqual(
         testModel.i18ns['en-US'].text
+    );
+
+});
+
+test('Use Vivy I18n translate default zh title', () => {
+
+    const vivy = Vivy();
+
+    vivy.use(VivyI18n({
+        language: 'zh-CN'
+    }));
+
+    const store = vivy.createStore();
+    store.registerModel(testModel);
+
+    expect(
+        translate('testModel/title')
+    ).toEqual(
+        testModel.i18ns['zh-CN'].title
+    );
+
+});
+
+test('Use Vivy I18n translate default zh text', () => {
+
+    const vivy = Vivy();
+
+    vivy.use(VivyI18n({
+        language: 'zh-CN'
+    }));
+
+    const store = vivy.createStore();
+    store.registerModel(testModel);
+
+    expect(
+        translate('testModel/text')
+    ).toEqual(
+        testModel.i18ns['zh-CN'].text
     );
 
 });
